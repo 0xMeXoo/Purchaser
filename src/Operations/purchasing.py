d85d1b35885dfac33e97893d0db7cf05775764d9
Gabbe-x <72690663+Gabbe-x@users.noreply.github.com>
2024-03-20 12:21:42 +0700

    Update purchasing.py

--- src/Operations/purchasing.py
+++ src/Operations/purchasing.py
@@ -6,8 +6,6 @@ from src.logger import cs_logger
 def purchase_lot(wallet):
     tier = tier_list[settings.tier - 1]
     cs_logger.info(f'Кошелек   {wallet.address}  |  Tier {tier.tier}')
-    if tier.tier == 1:
-        tier.purchase_timing(wallet)
     while tier.buy_limit > wallet.txn_num[tier.title]:
         tier.purchase(wallet)
         lots_remain = tier.check_lots()
